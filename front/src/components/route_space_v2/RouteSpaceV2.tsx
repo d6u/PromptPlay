0b7f323ef1f97101ef88b858ea0047d911f41c60
Daiwei Lu <daiweilu123@gmail.com>
2023-08-21 12:35:02 -0700

    Simple re-ordering

--- front/src/components/route_space_v2/RouteSpaceV2.tsx
+++ front/src/components/route_space_v2/RouteSpaceV2.tsx
@@ -2,7 +2,22 @@ import { gql } from "../../__generated__";
 import VariableMapArrow from "../icons/VaribleMapArrow";
 import "./RouteSpaceV2.css";
 import { useMutation, useQuery } from "@apollo/client";
+import {
+  DndContext,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  closestCenter,
+  useDraggable,
+  useDroppable,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
 import { Button } from "@mui/joy";
+import classNames from "classnames";
+import fp from "lodash/fp";
+import { nanoid } from "nanoid";
 import { ReactNode, useEffect, useState } from "react";
 
 const SPACE_V2_QUERY = gql(`
@@ -26,12 +41,14 @@ const UPDATE_SPACE_V2_MUTATION = gql(`
 `);
 
 type Block = {
+  id: string;
   input: { [key: string]: string } | string | null;
   code: string | null;
   output: { [key: string]: string } | string | null;
 };
 
 type BlockGroup = {
+  id: string;
   type: "root" | "repeat" | "alternative";
   blocks: Array<Block | BlockGroup>;
 };
@@ -57,6 +74,23 @@ export default function RouteSpaceV2({ spaceId }: { spaceId: string }) {
     }
   }, [query.data?.spaceV2?.content]);
 
+  const mouseSensor = useSensor(MouseSensor, {
+    // Require the mouse to move by 10 pixels before activating
+    activationConstraint: {
+      distance: 5,
+    },
+  });
+  const touchSensor = useSensor(TouchSensor, {
+    // Press delay of 250ms, with tolerance of 5px of movement
+    activationConstraint: {
+      delay: 250,
+      tolerance: 5,
+    },
+  });
+  const keyboardSensor = useSensor(KeyboardSensor);
+
+  const sensors = useSensors(mouseSensor, touchSensor, keyboardSensor);
+
   if (query.loading) {
     return <div>Loading...</div>;
   }
@@ -78,9 +112,28 @@ export default function RouteSpaceV2({ spaceId }: { spaceId: string }) {
         </div>
         <Button
           onClick={() => {
+            const newContent = {
+              id: nanoid(),
+              type: "root",
+              blocks: [],
+            };
+
+            updateSpaceV2({
+              variables: {
+                spaceId,
+                content: JSON.stringify(newContent),
+              },
+            });
+          }}
+        >
+          reset
+        </Button>
+        <Button
+          onClick={() => {
             let newContent: BlockGroup;
             if (content == null) {
               newContent = {
+                id: nanoid(),
                 type: "root",
                 blocks: [],
               };
@@ -89,6 +142,7 @@ export default function RouteSpaceV2({ spaceId }: { spaceId: string }) {
             }
 
             newContent.blocks.push({
+              id: nanoid(),
               input: {
                 scope_name: "argument_name",
               },
@@ -115,6 +169,7 @@ export default function RouteSpaceV2({ spaceId }: { spaceId: string }) {
             let newContent: BlockGroup;
             if (content == null) {
               newContent = {
+                id: nanoid(),
                 type: "root",
                 blocks: [],
               };
@@ -123,9 +178,11 @@ export default function RouteSpaceV2({ spaceId }: { spaceId: string }) {
             }
 
             newContent.blocks.push({
+              id: nanoid(),
               type: "repeat",
               blocks: [
                 {
+                  id: nanoid(),
                   input: {
                     messages: "messages",
                     message: "message",
@@ -153,13 +210,79 @@ export default function RouteSpaceV2({ spaceId }: { spaceId: string }) {
         </Button>
       </div>
       <div className="RouteSpaceV2_right">
-        {content && <BlockGroupComponent blockGroup={content} />}
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragStart={(event) => {}}
+          onDragEnd={(event) => {
+            const overId = event.over!.id as string;
+            const activeId = event.active!.id as string;
+            const activeBlock = content!.blocks.find(
+              ({ id }) => id === activeId
+            )!;
+
+            if (overId.startsWith("After:")) {
+              const positionId = overId.split(":")[1];
+              const newBlocks = fp.remove(
+                ({ id }) => id === activeId,
+                content!.blocks
+              );
+              const positionBlockIndex = newBlocks.findIndex(
+                ({ id }) => id === positionId
+              )!;
+
+              updateSpaceV2({
+                variables: {
+                  spaceId,
+                  content: JSON.stringify(
+                    fp.assign(content, {
+                      blocks: [
+                        ...newBlocks.slice(0, positionBlockIndex + 1),
+                        activeBlock,
+                        ...newBlocks.slice(positionBlockIndex + 1),
+                      ],
+                    })
+                  ),
+                },
+              });
+            } else if (overId.startsWith("First:")) {
+              const newBlocks = fp.remove(
+                ({ id }) => id === activeId,
+                content!.blocks
+              );
+
+              updateSpaceV2({
+                variables: {
+                  spaceId,
+                  content: JSON.stringify(
+                    fp.assign(content, {
+                      blocks: [activeBlock, ...newBlocks],
+                    })
+                  ),
+                },
+              });
+            }
+          }}
+        >
+          {content && <BlockGroupComponent blockGroup={content} />}
+        </DndContext>
       </div>
     </div>
   );
 }
 
-function BlockGroupComponent({ blockGroup }: { blockGroup: BlockGroup }) {
+function BlockGroupComponent({
+  blockGroup,
+  isParentDragging = false,
+}: {
+  blockGroup: BlockGroup;
+  isParentDragging?: boolean;
+}) {
+  const { isDragging, attributes, listeners, setNodeRef, transform } =
+    useDraggable({
+      id: blockGroup.id,
+    });
+
   let title: string;
   switch (blockGroup.type) {
     case "root":
@@ -173,21 +296,61 @@ function BlockGroupComponent({ blockGroup }: { blockGroup: BlockGroup }) {
       break;
   }
 
+  const content: ReactNode[] = [];
+  for (let i = 0; i < blockGroup.blocks.length; i++) {
+    const block = blockGroup.blocks[i];
+    if (i === 0) {
+      content.push(
+        <Gutter
+          key="first-gutter"
+          preItemId={`First:${blockGroup.id}`}
+          isDisabled={isDragging || isParentDragging}
+        />
+      );
+    }
+    if (isBlockGroup(block)) {
+      content.push(
+        <BlockGroupComponent
+          key={block.id}
+          blockGroup={block}
+          isParentDragging={isDragging || isParentDragging}
+        />
+      );
+    } else {
+      content.push(<BlockComponent key={block.id} block={block} />);
+    }
+    content.push(
+      <Gutter
+        key={`${block.id}-after-gutter`}
+        preItemId={`After:${block.id}`}
+        isDisabled={isDragging || isParentDragging}
+      />
+    );
+  }
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  };
+
   return (
-    <div className="RouteSpaceV2_group">
+    <div
+      className="RouteSpaceV2_group"
+      ref={setNodeRef}
+      style={style}
+      {...listeners}
+      {...attributes}
+    >
       <div className="RouteSpaceV2_group_title">{title}</div>
-      {blockGroup.blocks?.map((block, index) => {
-        if (isBlockGroup(block)) {
-          return <BlockGroupComponent key={index} blockGroup={block} />;
-        } else {
-          return <BlockComponent key={index} block={block} />;
-        }
-      })}
+      <div className="RouteSpaceV2_group_blocks">{content}</div>
     </div>
   );
 }
 
 function BlockComponent({ block }: { block: Block }) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: block.id,
+  });
+
   const inputChips: ReactNode[] = [];
   if (isObject(block.input)) {
     for (const [nameOnScope, argumentName] of Object.entries(block.input)) {
@@ -268,14 +431,46 @@ function BlockComponent({ block }: { block: Block }) {
     );
   }
 
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  };
+
   return (
-    <div className="RouteSpaceV2_block">
+    <div
+      className="RouteSpaceV2_block"
+      ref={setNodeRef}
+      style={style}
+      {...listeners}
+      {...attributes}
+    >
       <div className="RouteSpaceV2_block_input">{inputChips}</div>
       <div className="RouteSpaceV2_block_code">
         <pre>{block.code}</pre>
       </div>
       <div className="RouteSpaceV2_block_output">{outputChips}</div>
     </div>
+  );
+}
+
+function Gutter({
+  preItemId,
+  isDisabled,
+}: {
+  preItemId: string;
+  isDisabled: boolean;
+}) {
+  const { isOver, setNodeRef } = useDroppable({
+    id: preItemId,
+    disabled: isDisabled,
+  });
+
+  return (
+    <div
+      className={classNames("RouteSpaceV2_gutter", {
+        RouteSpaceV2_gutter_over: isOver,
+      })}
+      ref={setNodeRef}
+    ></div>
   );
 }
 
