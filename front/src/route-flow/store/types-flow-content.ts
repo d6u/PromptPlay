7c6a6a960415ce32c80cc7f84debe8b2b9a25f8b
Daiwei Lu <daiweilu123@gmail.com>
2023-11-15 18:48:12 -0800

    Update ChatCompletion with new OpenAI models

--- front/src/route-flow/store/types-flow-content.ts
+++ front/src/route-flow/store/types-flow-content.ts
@@ -111,8 +111,14 @@ export type ChatGPTChatCompletionNodeConfig = NodeConfigCommon & {
 };
 
 export enum OpenAIChatModel {
-  GPT3_5_TURBO = "gpt-3.5-turbo",
-  GPT4 = "gpt-4",
+  GPT_4_1106_PREVIEW = "gpt-4-1106-preview",
+  GPT_4 = "gpt-4",
+  GPT_4_32K = "gpt-4-32k",
+  GPT_4_0613 = "gpt-4-0613",
+  GPT_4_32K_0613 = "gpt-4-32k-0613",
+  GPT_3_5_TURBO_1106 = "gpt-3.5-turbo-1106",
+  GPT_3_5_TURBO = "gpt-3.5-turbo",
+  GPT_3_5_TURBO_16K = "gpt-3.5-turbo-16k",
 }
 
 // TextTemplate
