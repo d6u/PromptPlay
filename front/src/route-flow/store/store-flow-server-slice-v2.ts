cb3059a32b698d8af0a6e593c50274d69212bcd1
Daiwei Lu <daiweilu123@gmail.com>
2023-11-16 22:00:04 -0800

    WIP ChangeEvent

--- front/src/route-flow/store/store-flow-server-slice-v2.ts
+++ front/src/route-flow/store/store-flow-server-slice-v2.ts
@@ -1,5 +1,13 @@
+import { A } from "@mobily/ts-belt";
 import { produce } from "immer";
-import { OnConnect, OnEdgesChange, OnNodesChange } from "reactflow";
+import {
+  NodeChange,
+  OnConnect,
+  OnEdgesChange,
+  OnNodesChange,
+  applyEdgeChanges,
+  applyNodeChanges,
+} from "reactflow";
 import { Observable, Subscription, first, map, share } from "rxjs";
 import { OperationResult } from "urql";
 import { StateCreator } from "zustand";
@@ -11,6 +19,7 @@ import { SPACE_FLOW_QUERY } from "./graphql-flow";
 import {
   FlowContent,
   LocalEdge,
+  NodeConfig,
   NodeConfigs,
   VariableValueMap,
 } from "./types-flow-content";
@@ -120,9 +129,112 @@ export const createFlowServerSliceV2: StateCreator<
       fetchFlowSubscription = null;
     },
 
-    v2_onNodesChange() {},
-    v2_onEdgesChange() {},
-    v2_onConnect() {},
+    v2_onNodesChange(changes) {
+      const eventQueue: ChangeEvent[] = [
+        {
+          type: "NODES_CHANGE",
+          changes: changes,
+        },
+      ];
+
+      while (eventQueue.length > 0) {
+        const event = eventQueue.shift()!;
+
+        switch (event.type) {
+          case "NODES_CHANGE": {
+            const oldNodes = get().v2_nodes;
+            const newNodes = applyNodeChanges(
+              event.changes,
+              get().v2_nodes
+            ) as LocalNode[];
+
+            eventQueue.push(...processNodeChange(changes, oldNodes, newNodes));
+            break;
+          }
+          case "NODE_REMOVED": {
+            eventQueue.push(...processNodeRemoved(event.node));
+            break;
+          }
+          case "EDGE_REMOVED": {
+            console.log("EDGE_REMOVED", event.edge);
+            break;
+          }
+          case "NODE_CONFIG_REMOVED": {
+            console.log("NODE_CONFIG_REMOVED", event.nodeConfig);
+            break;
+          }
+        }
+      }
+
+      function processNodeChange(
+        changes: NodeChange[],
+        oldNodes: LocalNode[],
+        newNodes: LocalNode[]
+      ): ChangeEvent[] {
+        const events: ChangeEvent[] = [];
+
+        for (const change of changes) {
+          switch (change.type) {
+            case "remove": {
+              events.push({
+                type: "NODE_REMOVED",
+                node: oldNodes.find((n) => n.id === change.id)!,
+              });
+              break;
+            }
+            case "position":
+            case "add":
+            case "select":
+            case "dimensions":
+            case "reset": {
+              break;
+            }
+          }
+        }
+
+        set({ v2_nodes: newNodes });
+
+        return events;
+      }
+
+      function processNodeRemoved(removedNode: LocalNode): ChangeEvent[] {
+        const events: ChangeEvent[] = [];
+
+        // Process edges removal
+
+        const [acceptedEdges, rejectedEdges] = A.partition(
+          get().v2_edges,
+          (edge) =>
+            edge.source !== removedNode.id && edge.target !== removedNode.id
+        );
+
+        for (const edge of rejectedEdges) {
+          events.push({
+            type: "EDGE_REMOVED",
+            edge,
+          });
+        }
+
+        // Process nodeConfig removal
+
+        const removedNodeConfig = get().v2_nodeConfigs[removedNode.id]!;
+
+        const nodeConfigs = produce(get().v2_nodeConfigs, (draft) => {
+          delete draft[removedNode.id];
+        });
+
+        events.push({
+          type: "NODE_CONFIG_REMOVED",
+          nodeConfig: removedNodeConfig,
+        });
+
+        set({ v2_edges: acceptedEdges, v2_nodeConfigs: nodeConfigs });
+
+        return events;
+      }
+    },
+    v2_onEdgesChange(changes) {},
+    v2_onConnect(connection) {},
   };
 };
 
@@ -145,3 +257,21 @@ function assignLocalEdgeProperties(edges: LocalEdge[]): LocalEdge[] {
     }
   });
 }
+
+type ChangeEvent =
+  | {
+      type: "NODES_CHANGE";
+      changes: NodeChange[];
+    }
+  | {
+      type: "NODE_REMOVED";
+      node: LocalNode;
+    }
+  | {
+      type: "EDGE_REMOVED";
+      edge: LocalEdge;
+    }
+  | {
+      type: "NODE_CONFIG_REMOVED";
+      nodeConfig: NodeConfig;
+    };
