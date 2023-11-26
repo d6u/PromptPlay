1ca6607c8069bb29273d73e07ad81b84410991e4
Daiwei Lu <daiweilu123@gmail.com>
2023-11-25 20:11:38 -0800

    Split

--- front/src/components/route-flow/state/store-flow-state-types.ts
+++ front/src/components/route-flow/state/store-flow-state-types.ts
@@ -1,4 +1,10 @@
-import { NodeID } from "../../../models/v2-flow-content-types";
+import { LocalNode, NodeID } from "../../../models/v2-flow-content-types";
+import {
+  V3LocalEdge,
+  V3NodeConfigsDict,
+  V3VariableValueLookUpDict,
+  VariablesDict,
+} from "../../../models/v3-flow-content-types";
 import { CsvEvaluationPresetSlice } from "./slice-csv-evaluation-preset";
 import { SliceFlowContentV3 } from "./slice-flow-content-v3";
 import { RootSlice } from "./slice-root";
@@ -6,6 +12,18 @@ import { RootSlice } from "./slice-root";
 export type FlowState = SliceFlowContentV3 &
   RootSlice &
   CsvEvaluationPresetSlice;
+
+export type SliceFlowContentV3State = {
+  // Persist to server
+  nodes: LocalNode[];
+  edges: V3LocalEdge[];
+  nodeConfigsDict: V3NodeConfigsDict;
+  variablesDict: VariablesDict;
+  variableValueLookUpDicts: V3VariableValueLookUpDict[];
+  // Local
+  isFlowContentDirty: boolean;
+  isFlowContentSaving: boolean;
+};
 
 export type NodeAugments = Record<NodeID, NodeAugment>;
 
