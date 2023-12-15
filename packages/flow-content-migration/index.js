fcb200b270efe6cad6e5bf0d85dc97f82eac9eea
Daiwei Lu <daiweilu123@gmail.com>
2023-12-14 19:46:28 -0800

    Remove unused deps

---
+++ packages/flow-content-migration/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHi = void 0;
+function sayHi(name) {
+    console.log("hi");
+}
+exports.sayHi = sayHi;
--- server_nodejs/src/index.ts
+++ server_nodejs/src/index.ts
@@ -4,12 +4,9 @@ import "./utils/checkEnvVar.js";
 import cookieSession from "cookie-session";
 import cors from "cors";
 import express from "express";
-import { sayHi } from "flow-content-migration";
 import serverless from "serverless-http";
 import setupAuth from "./routesAuth.js";
 import setupGraphql from "./routesGraphql.js";
-
-sayHi("a");
 
 const app = express();
 
