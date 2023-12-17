dc94ab6f06b0264aff59ed9c4662b9599cea02e9
Daiwei Lu <daiweilu123@gmail.com>
2023-12-17 11:23:55 -0800

    Script to deploy frontend assets

---
+++ scripts/deploy-frontend-assets.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+pushd front
+
+printf "\n>>> Refreshing dependencies for front...\n"
+rm -rfv dist | sed 's/\([^/]*\)\/.*$/\1/' | sort | uniq
+
+printf "\n>>> Bundling and deploying the front...\n"
+pnpm run build -m $VITE_MODE_NAME
+aws s3 sync dist s3://cloudfront-$FRONTEND_DOMAIN_NAME-frontend \
+  --exclude "*.DS_Store" \
+  --delete
+
+popd
