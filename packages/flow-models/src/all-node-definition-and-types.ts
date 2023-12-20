7da20656095b290b381c0eb65235f0419729d182
Daiwei Lu <daiweilu123@gmail.com>
2023-12-20 00:13:12 -0800

    Add getNodeDefinitionForNodeTypeName()

--- packages/flow-models/src/all-node-definition-and-types.ts
+++ packages/flow-models/src/all-node-definition-and-types.ts
@@ -1,11 +1,33 @@
-import { V3ChatGPTChatCompletionNodeConfig } from './nodes/chatgpt-chat-completion-node';
-import { V3ChatGPTMessageNodeConfig } from './nodes/chatgpt-message-node';
-import { V3ElevenLabsNodeConfig } from './nodes/elevenlabs-node';
-import { V3HuggingFaceInferenceNodeConfig } from './nodes/huggingface-inference-node';
-import { V3InputNodeConfig } from './nodes/input-node';
-import { V3JavaScriptFunctionNodeConfig } from './nodes/javascript-function-node';
-import { V3OutputNodeConfig } from './nodes/output-node';
-import { V3TextTemplateNodeConfig } from './nodes/text-template-node';
+import { NodeDefinition } from './common/node-definition-base-types';
+import {
+  CHATGPT_CHAT_COMPLETION_NODE_DEFINITION,
+  V3ChatGPTChatCompletionNodeConfig,
+} from './nodes/chatgpt-chat-completion-node';
+import {
+  CHATGPT_MESSAGE_NODE_DEFINITION,
+  V3ChatGPTMessageNodeConfig,
+} from './nodes/chatgpt-message-node';
+import {
+  ELEVENLABS_NODE_DEFINITION,
+  V3ElevenLabsNodeConfig,
+} from './nodes/elevenlabs-node';
+import {
+  HUGGINGFACE_INFERENCE_NODE_DEFINITION,
+  V3HuggingFaceInferenceNodeConfig,
+} from './nodes/huggingface-inference-node';
+import { INPUT_NODE_DEFINITION, V3InputNodeConfig } from './nodes/input-node';
+import {
+  JAVASCRIPT_NODE_DEFINITION,
+  V3JavaScriptFunctionNodeConfig,
+} from './nodes/javascript-function-node';
+import {
+  OUTPUT_NODE_DEFINITION,
+  V3OutputNodeConfig,
+} from './nodes/output-node';
+import {
+  TEXT_TEMPLATE_NODE_DEFINITION,
+  V3TextTemplateNodeConfig,
+} from './nodes/text-template-node';
 
 export type V3NodeConfig =
   | V3InputNodeConfig
@@ -26,3 +48,23 @@ export type NodeTypeName =
   | V3TextTemplateNodeConfig['type']
   | V3HuggingFaceInferenceNodeConfig['type']
   | V3ElevenLabsNodeConfig['type'];
+
+const MAP = {
+  [INPUT_NODE_DEFINITION.nodeTypeName]: INPUT_NODE_DEFINITION,
+  [OUTPUT_NODE_DEFINITION.nodeTypeName]: OUTPUT_NODE_DEFINITION,
+  [JAVASCRIPT_NODE_DEFINITION.nodeTypeName]: JAVASCRIPT_NODE_DEFINITION,
+  [CHATGPT_MESSAGE_NODE_DEFINITION.nodeTypeName]:
+    CHATGPT_MESSAGE_NODE_DEFINITION,
+  [CHATGPT_CHAT_COMPLETION_NODE_DEFINITION.nodeTypeName]:
+    CHATGPT_CHAT_COMPLETION_NODE_DEFINITION,
+  [TEXT_TEMPLATE_NODE_DEFINITION.nodeTypeName]: TEXT_TEMPLATE_NODE_DEFINITION,
+  [HUGGINGFACE_INFERENCE_NODE_DEFINITION.nodeTypeName]:
+    HUGGINGFACE_INFERENCE_NODE_DEFINITION,
+  [ELEVENLABS_NODE_DEFINITION.nodeTypeName]: ELEVENLABS_NODE_DEFINITION,
+};
+
+export function getNodeDefinitionForNodeTypeName(
+  type: NodeTypeName,
+): NodeDefinition {
+  return MAP[type];
+}
--- packages/flow-models/src/v3-flow-utils.ts
+++ packages/flow-models/src/v3-flow-utils.ts
@@ -1,16 +1,10 @@
 import randomId from 'common-utils/randomId';
-import { NodeTypeName, V3NodeConfig } from './all-node-definition-and-types';
-import { NodeID, V3VariableID } from './basic-types';
 import {
-  CHATGPT_CHAT_COMPLETION_NODE_DEFINITION,
-  CHATGPT_MESSAGE_NODE_DEFINITION,
-  ELEVENLABS_NODE_DEFINITION,
-  HUGGINGFACE_INFERENCE_NODE_DEFINITION,
-  INPUT_NODE_DEFINITION,
-  JAVASCRIPT_NODE_DEFINITION,
-  OUTPUT_NODE_DEFINITION,
-  TEXT_TEMPLATE_NODE_DEFINITION,
-} from './nodes';
+  NodeTypeName,
+  V3NodeConfig,
+  getNodeDefinitionForNodeTypeName,
+} from './all-node-definition-and-types';
+import { NodeID, V3VariableID } from './basic-types';
 import {
   LocalNode,
   ServerNode,
@@ -35,30 +29,9 @@ export function createNodeConfig(node: LocalNode): {
   nodeConfig: V3NodeConfig;
   variableConfigList: Variable[];
 } {
-  switch (node.type) {
-    case INPUT_NODE_DEFINITION.nodeTypeName:
-      return INPUT_NODE_DEFINITION.createDefaultNodeConfig(node);
-    case OUTPUT_NODE_DEFINITION.nodeTypeName:
-      return OUTPUT_NODE_DEFINITION.createDefaultNodeConfig(node);
-    case JAVASCRIPT_NODE_DEFINITION.nodeTypeName:
-      return JAVASCRIPT_NODE_DEFINITION.createDefaultNodeConfig(node);
-    case CHATGPT_MESSAGE_NODE_DEFINITION.nodeTypeName:
-      return CHATGPT_MESSAGE_NODE_DEFINITION.createDefaultNodeConfig(node);
-    case CHATGPT_CHAT_COMPLETION_NODE_DEFINITION.nodeTypeName:
-      return CHATGPT_CHAT_COMPLETION_NODE_DEFINITION.createDefaultNodeConfig(
-        node,
-      );
-    case TEXT_TEMPLATE_NODE_DEFINITION.nodeTypeName:
-      return TEXT_TEMPLATE_NODE_DEFINITION.createDefaultNodeConfig(node);
-    case HUGGINGFACE_INFERENCE_NODE_DEFINITION.nodeTypeName:
-      return HUGGINGFACE_INFERENCE_NODE_DEFINITION.createDefaultNodeConfig(
-        node,
-      );
-    case ELEVENLABS_NODE_DEFINITION.nodeTypeName:
-      return ELEVENLABS_NODE_DEFINITION.createDefaultNodeConfig(node);
-    default:
-      throw new Error(`Unknown node type: ${node.type}`);
-  }
+  return getNodeDefinitionForNodeTypeName(node.type).createDefaultNodeConfig(
+    node,
+  );
 }
 
 export function asV3VariableID(id: VariableID | string): V3VariableID {
