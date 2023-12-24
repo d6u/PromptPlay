53ad2cdc0d60675a382e05eea243959228e3ffb3
Daiwei Lu <daiweilu123@gmail.com>
2023-12-24 14:17:09 -0800

    Update frontend deploy script

--- scripts/deploy-frontend-assets.sh
+++ scripts/deploy-frontend-assets.sh
@@ -8,6 +8,8 @@ pushd front
 printf "\n>>> Refreshing dependencies for front...\n"
 rm -rfv dist | sed 's/\([^/]*\)\/.*$/\1/' | sort | uniq
 
+pnpm version major
+
 printf "\n>>> Bundling and deploying the front...\n"
 pnpm run build -m $VITE_MODE_NAME
 aws s3 sync dist s3://cloudfront-$FRONTEND_DOMAIN_NAME-frontend \
