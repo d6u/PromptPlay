f726bf61f22b2210bc8b03ba50642566f930824f
Daiwei Lu <daiweilu123@gmail.com>
2024-01-10 21:37:37 -0800

    Remove un-used query logic

--- front/src/components/route-root/RouteRoot.tsx
+++ front/src/components/route-root/RouteRoot.tsx
@@ -1,18 +1,8 @@
 import styled from '@emotion/styled';
-import { useEffect } from 'react';
-import { Outlet, useSearchParams } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from '../common/header/Header';
 
 export default function RouteRoot() {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  useEffect(() => {
-    if (searchParams.has('new_user')) {
-      searchParams.delete('new_user');
-      setSearchParams(searchParams);
-    }
-  }, [searchParams, setSearchParams]);
-
   return (
     <Container>
       <Header />
