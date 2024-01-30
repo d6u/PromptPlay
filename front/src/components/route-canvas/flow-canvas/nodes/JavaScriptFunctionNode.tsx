114fdeed68109a1bc5bfe3515f42addeabf65ee3
Daiwei Lu <daiweilu123@gmail.com>
2024-01-29 21:58:54 -0800

    Refactor JavaScriptFunctionNode

--- front/src/components/route-canvas/flow-canvas/nodes/JavaScriptFunctionNode.tsx
+++ front/src/components/route-canvas/flow-canvas/nodes/JavaScriptFunctionNode.tsx
@@ -8,81 +8,35 @@ import {
   V3JavaScriptFunctionNodeConfig,
 } from 'flow-models';
 import { useContext, useMemo, useState } from 'react';
-import { Position, useNodeId, useUpdateNodeInternals } from 'reactflow';
-import invariant from 'tiny-invariant';
-import { useStore } from 'zustand';
+import { useNodeId } from 'reactflow';
 import RouteFlowContext from '../../../route-flow/common/RouteFlowContext';
 import TextareaReadonly from '../../../route-flow/common/TextareaReadonly';
 import {
   CopyIcon,
   LabelWithIconContainer,
 } from '../../../route-flow/common/flow-common';
-import { useStoreFromFlowStoreContext } from '../../../route-flow/store/FlowStoreContext';
-import {
-  selectConditionTarget,
-  selectVariables,
-} from '../../../route-flow/store/state-utils';
-import AddVariableButton from './node-common/AddVariableButton';
-import HeaderSection from './node-common/HeaderSection';
-import NodeBox, { NodeState } from './node-common/NodeBox';
-import NodeInputModifyRow from './node-common/NodeInputModifyRow';
-import NodeOutputRow from './node-common/NodeOutputRow';
-import {
-  ConditionTargetHandle,
-  InputHandle,
-  OutputHandle,
-  Section,
-  SmallSection,
-} from './node-common/node-common';
-import {
-  calculateInputHandleTop,
-  calculateOutputHandleBottom,
-} from './node-common/utils';
+import { useFlowStore } from '../../../route-flow/store/FlowStoreContext';
+import { selectVariables } from '../../../route-flow/store/state-utils';
+import ReactFlowNode from '../nodeV2/ReactFlowNode';
+import { Section } from './node-common/node-common';
 
 export default function JavaScriptFunctionNode() {
-  const nodeId = useNodeId() as NodeID;
-  const updateNodeInternals = useUpdateNodeInternals();
-
   const { isCurrentUserOwner } = useContext(RouteFlowContext);
-  const flowStore = useStoreFromFlowStoreContext();
 
-  // SECTION: Select state from store
+  const nodeId = useNodeId() as NodeID;
 
-  const nodeConfigsDict = useStore(flowStore, (s) => s.nodeConfigsDict);
-  const variablesDict = useStore(flowStore, (s) => s.variablesDict);
-  const updateNodeConfig = useStore(flowStore, (s) => s.updateNodeConfig);
-  const removeNode = useStore(flowStore, (s) => s.removeNode);
-  const addVariable = useStore(flowStore, (s) => s.addVariable);
-  const updateVariable = useStore(flowStore, (s) => s.updateVariable);
-  const removeVariable = useStore(flowStore, (s) => s.removeVariable);
-  const nodeMetadataDict = useStore(flowStore, (s) => s.nodeMetadataDict);
-  const defaultVariableValueMap = useStore(flowStore, (s) =>
-    s.getDefaultVariableValueLookUpDict(),
-  );
-
-  // !SECTION
+  const nodeConfigsDict = useFlowStore((s) => s.nodeConfigsDict);
+  const variablesDict = useFlowStore((s) => s.variablesDict);
+  const updateNodeConfig = useFlowStore((s) => s.updateNodeConfig);
 
   const inputs = useMemo(() => {
     return selectVariables(nodeId, ConnectorType.NodeInput, variablesDict);
-  }, [nodeId, variablesDict]);
-
-  const outputs = useMemo(() => {
-    return selectVariables(nodeId, ConnectorType.NodeOutput, variablesDict);
   }, [nodeId, variablesDict]);
 
   const nodeConfig = useMemo(
     () => nodeConfigsDict[nodeId] as V3JavaScriptFunctionNodeConfig | undefined,
     [nodeConfigsDict, nodeId],
   );
-
-  const augment = useMemo(
-    () => nodeMetadataDict[nodeId],
-    [nodeMetadataDict, nodeId],
-  );
-
-  const conditionTarget = useMemo(() => {
-    return selectConditionTarget(nodeId, variablesDict);
-  }, [variablesDict, nodeId]);
 
   // It's OK to force unwrap here because nodeConfig will be undefined only
   // when Node is being deleted.
@@ -94,123 +48,54 @@ export default function JavaScriptFunctionNode() {
     return null;
   }
 
-  invariant(conditionTarget != null);
-
   const functionDefinitionPrefix = `async function (${inputs
     .map((v) => v.name)
     .join(', ')}) {`;
 
   return (
-    <>
-      <ConditionTargetHandle controlId={conditionTarget.id} />
-      {inputs.map((input, i) => (
-        <InputHandle
-          key={i}
-          type="target"
-          id={input.id}
-          position={Position.Left}
-          style={{
-            top: calculateInputHandleTop(i - (isCurrentUserOwner ? 0 : 1)),
-          }}
-        />
-      ))}
-      <NodeBox
-        nodeType={NodeType.JavaScriptFunctionNode}
-        state={
-          augment?.isRunning
-            ? NodeState.Running
-            : augment?.hasError
-              ? NodeState.Error
-              : NodeState.Idle
-        }
-      >
-        <HeaderSection
-          isCurrentUserOwner={isCurrentUserOwner}
-          title="JavaScript"
-          onClickRemove={() => {
-            removeNode(nodeId);
-          }}
-        />
-        {isCurrentUserOwner && (
-          <SmallSection>
-            <AddVariableButton
+    <ReactFlowNode
+      nodeType={NodeType.JavaScriptFunctionNode}
+      nodeTitle="JavaScript"
+      allowAddVariable={true}
+    >
+      <Section>
+        <FormControl>
+          <LabelWithIconContainer>
+            <FormLabel>
+              <code>{functionDefinitionPrefix}</code>
+            </FormLabel>
+            <CopyIcon
               onClick={() => {
-                addVariable(nodeId, ConnectorType.NodeInput, inputs.length);
-                updateNodeInternals(nodeId);
-              }}
-            />
-          </SmallSection>
-        )}
-        <Section>
-          {inputs.map((input, i) => (
-            <NodeInputModifyRow
-              key={input.id}
-              name={input.name}
-              isReadOnly={!isCurrentUserOwner}
-              onConfirmNameChange={(name) => {
-                updateVariable(input.id, { name });
-              }}
-              onRemove={() => {
-                removeVariable(input.id);
-                updateNodeInternals(nodeId);
-              }}
-            />
-          ))}
-        </Section>
-        <Section>
-          <FormControl>
-            <LabelWithIconContainer>
-              <FormLabel>
-                <code>{functionDefinitionPrefix}</code>
-              </FormLabel>
-              <CopyIcon
-                onClick={() => {
-                  navigator.clipboard.writeText(`${functionDefinitionPrefix}
+                navigator.clipboard.writeText(`${functionDefinitionPrefix}
   ${javaScriptCode.split('\n').join('\n  ')}
 }`);
-                }}
-              />
-            </LabelWithIconContainer>
-            {isCurrentUserOwner ? (
-              <Textarea
-                sx={{ fontFamily: 'var(--font-family-mono)' }}
-                minRows={6}
-                placeholder="Write JavaScript here"
-                value={javaScriptCode}
-                onChange={(e) => {
-                  setJavaScriptCode(e.target.value);
-                }}
-                onKeyDown={(e) => {
-                  if ((e.metaKey || e.ctrlKey) && e.key === 'Enter') {
-                    updateNodeConfig(nodeId, { javaScriptCode });
-                  }
-                }}
-                onBlur={() => {
+              }}
+            />
+          </LabelWithIconContainer>
+          {isCurrentUserOwner ? (
+            <Textarea
+              sx={{ fontFamily: 'var(--font-family-mono)' }}
+              minRows={6}
+              placeholder="Write JavaScript here"
+              value={javaScriptCode}
+              onChange={(e) => {
+                setJavaScriptCode(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if ((e.metaKey || e.ctrlKey) && e.key === 'Enter') {
                   updateNodeConfig(nodeId, { javaScriptCode });
-                }}
-              />
-            ) : (
-              <TextareaReadonly value={javaScriptCode} minRows={6} isCode />
-            )}
-            <code style={{ fontSize: 12 }}>{'}'}</code>
-          </FormControl>
-        </Section>
-        <Section>
-          <NodeOutputRow
-            id={outputs[0].id}
-            name={outputs[0].name}
-            value={defaultVariableValueMap[outputs[0].id]}
-          />
-        </Section>
-      </NodeBox>
-      <OutputHandle
-        type="source"
-        id={outputs[0].id}
-        position={Position.Right}
-        style={{
-          bottom: calculateOutputHandleBottom(outputs.length - 1),
-        }}
-      />
-    </>
+                }
+              }}
+              onBlur={() => {
+                updateNodeConfig(nodeId, { javaScriptCode });
+              }}
+            />
+          ) : (
+            <TextareaReadonly value={javaScriptCode} minRows={6} isCode />
+          )}
+          <code style={{ fontSize: 12 }}>{'}'}</code>
+        </FormControl>
+      </Section>
+    </ReactFlowNode>
   );
 }
