df0285b5b299ee06747b95e77ba6bb9d7e6ffb24
Daiwei Lu <daiweilu123@gmail.com>
2024-02-15 19:07:26 -0800

    Deploy front v9

--- scripts/deploy-frontend-assets.sh
+++ scripts/deploy-frontend-assets.sh
@@ -3,12 +3,31 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+no_bump_version=false
+
+# Loop through all the positional parameters
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --no-bump)
+    no_bump_version=true
+    shift # Remove --no-bump from processing
+    ;;
+  *) # Unknown option
+    echo "Unknown option: $1"
+    exit 1
+    ;;
+  esac
+done
+
 pushd front
 
 printf "\n>>> Refreshing dependencies for front...\n"
 rm -rfv dist | sed 's/\([^/]*\)\/.*$/\1/' | sort | uniq
 
-pnpm version major
+if ! $no_bump_version; then
+  echo "Bumping front package version..."
+  pnpm version major
+fi
 
 printf "\n>>> Bundling and deploying the front...\n"
 pnpm run build -m $VITE_MODE_NAME
