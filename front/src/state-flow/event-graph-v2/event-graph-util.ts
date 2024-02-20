cfe976256f09e4e80ac10092d6447e9ae86d7f06
Daiwei Lu <daiweilu123@gmail.com>
2024-02-19 20:12:59 -0800

    Improve event graph util with selector argument

--- front/src/state-flow/event-graph-v2/event-graph-util.ts
+++ front/src/state-flow/event-graph-v2/event-graph-util.ts
@@ -1,41 +1,101 @@
 import { Draft } from 'immer';
+
+import { G } from '@mobily/ts-belt';
+import invariant from 'tiny-invariant';
 import { SliceFlowContentV3State } from '../types';
 
 type State = SliceFlowContentV3State;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type BaseEvent = { type: any };
 
-type EventHandler<TIn, TOut> = (state: Draft<State>, event: TIn) => TOut[];
+type EventSelector<TIn extends BaseEvent> = (event: BaseEvent) => event is TIn;
 
-type EventProcessor<TIn> = (state: Draft<State>, event: TIn) => void;
+type HandleEventFun<TIn, TOut> = (state: Draft<State>, event: TIn) => TOut[];
 
-type HandlerHolder<TIn, TOut> = {
-  processEvent: EventProcessor<TIn>;
-  register: (downstreamProcessor: EventProcessor<TOut>) => void;
-};
+type Handler<TIn> = (state: Draft<State>, event: TIn) => void;
 
-export function createHandlerHolder<TIn, TOut>(
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  deps: HandlerHolder<any, TIn>[],
-  handleEvent: EventHandler<TIn, TOut>,
-): HandlerHolder<TIn, TOut> {
-  const DOWNSTREAM_PROCESSORS: EventProcessor<TOut>[] = [];
+function createHandler<TIn, TOut>(
+  handleEvent: HandleEventFun<TIn, TOut>,
+): Handler<TIn>;
+function createHandler<TIn, TOut>(
+  handleEvent: HandleEventFun<TIn, TOut>,
+  nextHandlers: Handler<TOut>[],
+): Handler<TIn>;
+function createHandler<TSelect extends BaseEvent, TOut>(
+  selector: EventSelector<TSelect>,
+  handleEvent: HandleEventFun<TSelect, TOut>,
+): Handler<BaseEvent>;
+function createHandler<TSelect extends BaseEvent, TOut>(
+  selector: EventSelector<TSelect>,
+  handleEvent: HandleEventFun<TSelect, TOut>,
+  nextHandlers: Handler<TOut>[],
+): Handler<BaseEvent>;
+function createHandler<TIn, TSelect extends BaseEvent, TOut>(
+  handleEventOrSelector: HandleEventFun<TIn, TOut> | EventSelector<TSelect>,
+  nextHandlersOrHandleEvent?: Handler<TOut>[] | HandleEventFun<TSelect, TOut>,
+  emptyOrNextHandlers?: Handler<TOut>[],
+): Handler<TIn> | Handler<BaseEvent> {
+  let selector: EventSelector<TSelect> | null;
+  let handleEvent: HandleEventFun<TIn, TOut> | HandleEventFun<TSelect, TOut>;
+  let nextHandlers: Handler<TOut>[];
 
-  function processEvent(state: State, event: TIn) {
-    const nextEvents = handleEvent(state, event);
-    for (const nextEvent of nextEvents) {
-      for (const eventProcessor of DOWNSTREAM_PROCESSORS) {
-        eventProcessor(state, nextEvent);
+  if (emptyOrNextHandlers != null) {
+    selector = handleEventOrSelector as EventSelector<TSelect>;
+    handleEvent = nextHandlersOrHandleEvent as HandleEventFun<TSelect, TOut>;
+    nextHandlers = emptyOrNextHandlers;
+  } else {
+    if (nextHandlersOrHandleEvent == null) {
+      selector = null;
+      handleEvent = handleEventOrSelector as HandleEventFun<TIn, TOut>;
+      nextHandlers = [];
+    } else {
+      if (G.isArray(nextHandlersOrHandleEvent)) {
+        selector = null;
+        handleEvent = handleEventOrSelector as HandleEventFun<TIn, TOut>;
+        nextHandlers = nextHandlersOrHandleEvent;
+      } else {
+        selector = handleEventOrSelector as EventSelector<TSelect>;
+        handleEvent = nextHandlersOrHandleEvent;
+        nextHandlers = [];
       }
     }
   }
 
-  deps.forEach((dep) => {
-    dep.register(processEvent);
-  });
+  // NOTE: Below is a bit verbose. But it's for working with TypeScript.
 
-  return {
-    processEvent,
-    register(downstreamProcessor) {
-      DOWNSTREAM_PROCESSORS.push(downstreamProcessor);
-    },
-  };
+  if (selector == null) {
+    return function handleEventWrapper(state: State, event: TIn) {
+      const nextEvents = (handleEvent as HandleEventFun<TIn, TOut>)(
+        state,
+        event,
+      );
+
+      for (const nextEvent of nextEvents) {
+        for (const handler of nextHandlers) {
+          handler(state, nextEvent);
+        }
+      }
+    };
+  } else {
+    return function handleEventWrapper(state: State, event: BaseEvent) {
+      invariant(selector != null, 'selector is not null');
+
+      if (!selector(event)) {
+        return;
+      }
+
+      const nextEvents = (handleEvent as HandleEventFun<TSelect, TOut>)(
+        state,
+        event,
+      );
+
+      for (const nextEvent of nextEvents) {
+        for (const handler of nextHandlers) {
+          handler(state, nextEvent);
+        }
+      }
+    };
+  }
 }
+
+export { createHandler };
