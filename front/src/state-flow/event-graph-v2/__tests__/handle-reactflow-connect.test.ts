d2e18a884494ceec9e5acd3e53bda232f691309a
Daiwei Lu <daiweilu123@gmail.com>
2024-02-22 19:32:45 -0800

    Add test case for handleReactFlowConnect

--- front/src/state-flow/event-graph-v2/__tests__/handle-reactflow-connect.test.ts
+++ front/src/state-flow/event-graph-v2/__tests__/handle-reactflow-connect.test.ts
@@ -13,38 +13,17 @@ import {
 } from 'flow-models';
 
 import { ChangeEventType } from 'state-flow/event-graph/event-graph-types';
-import { BatchTestTab } from 'state-flow/types';
 
-import { handleReactFlowConnectEvent } from '../handle-reactflow-connect';
+import { BaseEvent, State } from '../event-graph-util';
+import {
+  handleReactFlowConnect,
+  handleReactFlowConnectEvent,
+} from '../handle-reactflow-connect';
+import { MOCK_STATE } from './fixture';
 
 vi.stubGlobal('alert', () => {});
 
-const MOCK_STATE = {
-  // Persist to server
-  nodes: [],
-  edges: [],
-  nodeConfigsDict: {},
-  variablesDict: {},
-  variableValueLookUpDicts: [{}],
-  // Local
-  isFlowContentDirty: false,
-  isFlowContentSaving: false,
-
-  selectedBatchTestTab: BatchTestTab.RunTests,
-
-  csvModeSelectedPresetId: null,
-  csvEvaluationIsLoading: false,
-
-  // Local data
-  csvStr: '',
-  csvEvaluationConfigContent: {
-    repeatTimes: 1,
-    concurrencyLimit: 2,
-    variableIdToCsvColumnIndexMap: {},
-    runOutputTable: [],
-    runMetadataTable: [],
-  },
-};
+// ANCHOR: Test cases for handleReactFlowConnectEvent
 
 test('handleReactFlowConnectEvent ignores self connect', () => {
   const state = produce(MOCK_STATE, (draft) => {
@@ -329,6 +308,139 @@ test('handleReactFlowConnectEvent add condition', () => {
         sourceHandle: '1' as ConnectorID,
         targetHandle: '2' as ConnectorID,
         style: expect.any(Object),
+      },
+    ],
+  });
+});
+
+// ANCHOR: Test cases for handleReactFlowConnect
+
+test('handleReactFlowConnect should replace edge', () => {
+  const prevState: State = {
+    ...MOCK_STATE,
+    nodes: [
+      {
+        id: 'ZUhTs',
+        type: 'InputNode',
+        position: {
+          x: 228,
+          y: 148,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 132,
+      },
+      {
+        id: 'Is8Op',
+        type: 'OutputNode',
+        position: {
+          x: 690,
+          y: 159,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 132,
+      },
+      {
+        id: 'WHqYI',
+        type: 'InputNode',
+        position: {
+          x: 229,
+          y: 327,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 132,
+      },
+    ],
+    edges: [
+      {
+        source: 'ZUhTs',
+        sourceHandle: 'ZUhTs/aPZ3h',
+        target: 'Is8Op',
+        targetHandle: 'Is8Op/5TUFT',
+        id: 'lfx3a',
+        style: {
+          strokeWidth: 2,
+        },
+      },
+    ],
+    nodeConfigsDict: {
+      ZUhTs: {
+        nodeId: 'ZUhTs',
+        type: 'InputNode',
+      },
+      Is8Op: {
+        nodeId: 'Is8Op',
+        type: 'OutputNode',
+      },
+      WHqYI: {
+        nodeId: 'WHqYI',
+        type: 'InputNode',
+      },
+    },
+    variablesDict: {
+      'ZUhTs/aPZ3h': {
+        type: 'FlowInput',
+        id: 'ZUhTs/aPZ3h',
+        nodeId: 'ZUhTs',
+        index: 0,
+        name: 'var1',
+        valueType: 'String',
+      },
+      'Is8Op/5TUFT': {
+        type: 'FlowOutput',
+        id: 'Is8Op/5TUFT',
+        nodeId: 'Is8Op',
+        index: 0,
+        name: 'var3',
+        valueType: 'String',
+      },
+      'WHqYI/p8a32': {
+        type: 'FlowInput',
+        id: 'WHqYI/p8a32',
+        nodeId: 'WHqYI',
+        index: 0,
+        name: 'var2',
+        valueType: 'String',
+      },
+    },
+    variableValueLookUpDicts: [
+      {
+        'ZUhTs/aPZ3h': null,
+        'Is8Op/5TUFT': null,
+        'WHqYI/p8a32': null,
+      },
+    ],
+  };
+
+  const nextState = produce(prevState, (draft) => {
+    handleReactFlowConnect(draft, {
+      type: ChangeEventType.RF_ON_CONNECT,
+      connection: {
+        source: 'WHqYI',
+        sourceHandle: 'WHqYI/p8a32',
+        target: 'Is8Op',
+        targetHandle: 'Is8Op/5TUFT',
+      },
+    } as BaseEvent);
+  });
+
+  expect(nextState).toEqual({
+    ...prevState,
+    edges: [
+      {
+        id: expect.any(String),
+        source: 'WHqYI',
+        sourceHandle: 'WHqYI/p8a32',
+        target: 'Is8Op',
+        targetHandle: 'Is8Op/5TUFT',
+        style: {
+          strokeWidth: 2,
+        },
       },
     ],
   });
--- front/src/state-flow/event-graph-v2/__tests__/update-variable-on-edge-replace.test.ts
+++ front/src/state-flow/event-graph-v2/__tests__/update-variable-on-edge-replace.test.ts
@@ -10,65 +10,137 @@ import {
 } from 'flow-models';
 
 import { ChangeEventType } from 'state-flow/event-graph/event-graph-types';
-import { BatchTestTab } from 'state-flow/types';
 
+import { State } from '../event-graph-util';
 import { handleEdgeReplacedEvent } from '../update-variable-on-edge-replace';
+import { MOCK_STATE } from './fixture';
 
 vi.stubGlobal('alert', () => {});
 
-const MOCK_STATE = {
-  // Persist to server
-  nodes: [],
-  edges: [],
-  nodeConfigsDict: {},
-  variablesDict: {},
-  variableValueLookUpDicts: [{}],
-  // Local
-  isFlowContentDirty: false,
-  isFlowContentSaving: false,
-
-  selectedBatchTestTab: BatchTestTab.RunTests,
-
-  csvModeSelectedPresetId: null,
-  csvEvaluationIsLoading: false,
-
-  // Local data
-  csvStr: '',
-  csvEvaluationConfigContent: {
-    repeatTimes: 1,
-    concurrencyLimit: 2,
-    variableIdToCsvColumnIndexMap: {},
-    runOutputTable: [],
-    runMetadataTable: [],
-  },
-};
-
 test('handleEdgeReplacedEvent ignores old and new source variables with the same value type', () => {
-  const prevState = {
+  const prevState: State = {
     ...MOCK_STATE,
-    variablesDict: {
-      a1: { type: ConnectorType.NodeOutput },
-      c1: { type: ConnectorType.NodeOutput },
-      b1: { type: ConnectorType.NodeInput },
+    nodes: [
+      {
+        id: 'ZUhTs',
+        type: 'InputNode',
+        position: {
+          x: 228,
+          y: 148,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 132,
+      },
+      {
+        id: 'Is8Op',
+        type: 'OutputNode',
+        position: {
+          x: 690,
+          y: 159,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 132,
+      },
+      {
+        id: 'WHqYI',
+        type: 'InputNode',
+        position: {
+          x: 229,
+          y: 327,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 132,
+      },
+    ],
+    edges: [
+      {
+        source: 'ZUhTs',
+        sourceHandle: 'ZUhTs/aPZ3h',
+        target: 'Is8Op',
+        targetHandle: 'Is8Op/5TUFT',
+        id: 'ZXnIZ',
+        style: {
+          strokeWidth: 2,
+        },
+      },
+    ],
+    nodeConfigsDict: {
+      ZUhTs: {
+        nodeId: 'ZUhTs',
+        type: 'InputNode',
+      },
+      Is8Op: {
+        nodeId: 'Is8Op',
+        type: 'OutputNode',
+      },
+      WHqYI: {
+        nodeId: 'WHqYI',
+        type: 'InputNode',
+      },
     },
+    variablesDict: {
+      'ZUhTs/aPZ3h': {
+        type: 'FlowInput',
+        id: 'ZUhTs/aPZ3h',
+        nodeId: 'ZUhTs',
+        index: 0,
+        name: 'var1',
+        valueType: 'String',
+      },
+      'Is8Op/5TUFT': {
+        type: 'FlowOutput',
+        id: 'Is8Op/5TUFT',
+        nodeId: 'Is8Op',
+        index: 0,
+        name: 'var3',
+        valueType: 'String',
+      },
+      'WHqYI/p8a32': {
+        type: 'FlowInput',
+        id: 'WHqYI/p8a32',
+        nodeId: 'WHqYI',
+        index: 0,
+        name: 'var2',
+        valueType: 'String',
+      },
+    },
+    variableValueLookUpDicts: [
+      {
+        'ZUhTs/aPZ3h': null,
+        'Is8Op/5TUFT': null,
+        'WHqYI/p8a32': null,
+      },
+    ],
   };
 
   const nextState = produce(prevState, (draft) => {
     const r = handleEdgeReplacedEvent(draft, {
       type: ChangeEventType.EDGE_REPLACED,
       oldEdge: {
-        id: 'e1' as EdgeID,
-        source: 'a' as NodeID,
-        sourceHandle: 'a1' as ConnectorID,
-        target: 'b' as NodeID,
-        targetHandle: 'b1' as ConnectorID,
+        source: 'ZUhTs',
+        sourceHandle: 'ZUhTs/aPZ3h',
+        target: 'Is8Op',
+        targetHandle: 'Is8Op/5TUFT',
+        id: 'ZXnIZ',
+        style: {
+          strokeWidth: 2,
+        },
       },
       newEdge: {
-        id: 'e2' as EdgeID,
-        source: 'c' as NodeID,
-        sourceHandle: 'c1' as ConnectorID,
-        target: 'b' as NodeID,
-        targetHandle: 'b1' as ConnectorID,
+        source: 'WHqYI',
+        sourceHandle: 'WHqYI/p8a32',
+        target: 'Is8Op',
+        targetHandle: 'Is8Op/5TUFT',
+        id: 'ZmY2F',
+        style: {
+          strokeWidth: 2,
+        },
       },
     });
 
