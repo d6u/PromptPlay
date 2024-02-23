097ad410a4399c85a87f115bf2b1468a76b9289d
Daiwei Lu <daiweilu123@gmail.com>
2024-02-22 20:17:22 -0800

    Add tests for addEdge function in reactflow.test.ts

--- front/src/state-flow/event-graph-v2/__tests__/reactflow.test.ts
+++ front/src/state-flow/event-graph-v2/__tests__/reactflow.test.ts
@@ -2,6 +2,11 @@ import { produce } from 'immer';
 import { Edge, addEdge } from 'reactflow';
 import { expect, test } from 'vitest';
 
+/**
+ * The reason for testing an external library's function to ensure that it has
+ * the implicit behavior that wasn't documented.
+ */
+
 test('addEdge should not mutate edges array', () => {
   const edges: Edge[] = [
     {
@@ -32,52 +37,7 @@ test('addEdge should not mutate edges array', () => {
   expect(sameEdges).toBe(edges);
 });
 
-test('addEdge should replace edge', () => {
-  const edges: Edge[] = [
-    {
-      source: 'ZUhTs',
-      sourceHandle: 'ZUhTs/aPZ3h',
-      target: 'Is8Op',
-      targetHandle: 'Is8Op/5TUFT',
-      id: 'lfx3a',
-      style: {
-        strokeWidth: 2,
-      },
-    },
-  ];
-
-  const newEdges = addEdge(
-    {
-      source: 'WHqYI',
-      sourceHandle: 'WHqYI/p8a32',
-      target: 'Is8Op',
-      targetHandle: 'Is8Op/5TUFT',
-    },
-    edges,
-  );
-
-  expect(newEdges).toEqual([
-    {
-      source: 'ZUhTs',
-      sourceHandle: 'ZUhTs/aPZ3h',
-      target: 'Is8Op',
-      targetHandle: 'Is8Op/5TUFT',
-      id: 'lfx3a',
-      style: {
-        strokeWidth: 2,
-      },
-    },
-    {
-      id: expect.any(String),
-      source: 'WHqYI',
-      sourceHandle: 'WHqYI/p8a32',
-      target: 'Is8Op',
-      targetHandle: 'Is8Op/5TUFT',
-    },
-  ]);
-});
-
-test('addEdge should ignore existing connection and not mutate edges array', () => {
+test('addEdge should ignore existing connection and return the same edges object', () => {
   const edges: Edge[] = [
     {
       source: 'ZUhTs',
