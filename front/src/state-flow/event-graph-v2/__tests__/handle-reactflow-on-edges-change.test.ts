f41826d8cbe9d7a0fb1644a84ac518fb4efcd61f
Daiwei Lu <daiweilu123@gmail.com>
2024-02-24 17:16:00 -0800

    Add test for removing multiple edges in handleReactFlowEdgesChange

--- front/src/state-flow/event-graph-v2/__tests__/handle-reactflow-on-edges-change.test.ts
+++ front/src/state-flow/event-graph-v2/__tests__/handle-reactflow-on-edges-change.test.ts
@@ -213,6 +213,184 @@ test('handleReactFlowEdgesChange should remove edge', () => {
   });
 });
 
+test('handleReactFlowEdgesChange should remove multiple edges', () => {
+  const prevState: State = {
+    ...MOCK_STATE,
+    nodes: [
+      {
+        id: 'HIbCf',
+        type: 'InputNode',
+        position: {
+          x: 606.5630634219496,
+          y: 144.8419333387866,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 132,
+        selected: false,
+        positionAbsolute: {
+          x: 606.5630634219496,
+          y: 144.8419333387866,
+        },
+        dragging: false,
+      },
+      {
+        id: 'sn268',
+        type: 'OutputNode',
+        position: {
+          x: 1439.0630634219494,
+          y: 131.50860000545333,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 169,
+        selected: false,
+        positionAbsolute: {
+          x: 1439.0630634219494,
+          y: 131.50860000545333,
+        },
+        dragging: false,
+      },
+      {
+        id: 'AkRxM',
+        type: 'TextTemplate',
+        position: {
+          x: 989.0630634219492,
+          y: 179.0086000054534,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 328,
+        selected: true,
+        positionAbsolute: {
+          x: 989.0630634219492,
+          y: 179.0086000054534,
+        },
+        dragging: false,
+      },
+    ],
+    edges: [
+      {
+        source: 'AkRxM',
+        sourceHandle: 'AkRxM/content',
+        target: 'sn268',
+        targetHandle: 'sn268/TweWr',
+        id: 'TSL0N',
+        style: {
+          strokeWidth: 2,
+        },
+        selected: true,
+      },
+      {
+        source: 'HIbCf',
+        sourceHandle: 'HIbCf/sCWR7',
+        target: 'AkRxM',
+        targetHandle: 'AkRxM/QCSCO',
+        id: '0GtZJ',
+        style: {
+          strokeWidth: 2,
+        },
+        selected: true,
+      },
+    ],
+    nodeConfigsDict: {
+      HIbCf: {
+        nodeId: 'HIbCf',
+        type: 'InputNode',
+      },
+      sn268: {
+        nodeId: 'sn268',
+        type: 'OutputNode',
+      },
+      AkRxM: {
+        nodeId: 'AkRxM',
+        type: 'TextTemplate',
+        content: 'Write a poem about {{topic}} in fewer than 20 words.',
+      },
+    },
+    variablesDict: {
+      'HIbCf/sCWR7': {
+        type: 'FlowInput',
+        id: 'HIbCf/sCWR7',
+        nodeId: 'HIbCf',
+        index: 0,
+        name: 'var1',
+        valueType: 'String',
+      },
+      'sn268/mt4IG': {
+        type: 'FlowOutput',
+        id: 'sn268/mt4IG',
+        nodeId: 'sn268',
+        index: 0,
+        name: 'var2',
+        valueType: 'String',
+      },
+      'sn268/TweWr': {
+        id: 'sn268/TweWr',
+        nodeId: 'sn268',
+        index: 1,
+        name: 'var3',
+        type: 'FlowOutput',
+        valueType: 'String',
+      },
+      'AkRxM/QCSCO': {
+        type: 'NodeInput',
+        id: 'AkRxM/QCSCO',
+        name: 'topic',
+        nodeId: 'AkRxM',
+        index: 0,
+        valueType: 'Unknown',
+      },
+      'AkRxM/content': {
+        type: 'NodeOutput',
+        id: 'AkRxM/content',
+        name: 'content',
+        nodeId: 'AkRxM',
+        index: 0,
+        valueType: 'Unknown',
+      },
+      'AkRxM/5qUcP': {
+        type: 'ConditionTarget',
+        id: 'AkRxM/5qUcP',
+        nodeId: 'AkRxM',
+      },
+    },
+    variableValueLookUpDicts: [
+      {
+        'HIbCf/sCWR7': null,
+        'sn268/mt4IG': null,
+        'sn268/TweWr': null,
+        'AkRxM/QCSCO': null,
+        'AkRxM/content': null,
+      },
+    ],
+  };
+
+  const nextState = produce(prevState, (draft) => {
+    handleReactFlowEdgesChange(draft, {
+      type: ChangeEventType.RF_EDGES_CHANGE,
+      changes: [
+        {
+          id: 'TSL0N',
+          type: 'remove',
+        },
+        {
+          id: '0GtZJ',
+          type: 'remove',
+        },
+      ],
+    } as BaseEvent);
+  });
+
+  expect(nextState).toEqual({
+    ...prevState,
+    edges: [],
+  });
+});
+
 test('handleReactFlowEdgesChange should remove edge and reset the value type of the target variable of the removed edge', () => {
   const prevState: State = {
     ...MOCK_STATE,
