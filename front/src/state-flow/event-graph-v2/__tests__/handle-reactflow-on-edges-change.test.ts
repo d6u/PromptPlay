62e26a0c1fa8e2730424fdfeec1959853ac80803
Daiwei Lu <daiweilu123@gmail.com>
2024-02-24 16:59:24 -0800

    Add test for handleReactFlowEdgesChange removing edge and resetting target variable value type

--- front/src/state-flow/event-graph-v2/__tests__/handle-reactflow-on-edges-change.test.ts
+++ front/src/state-flow/event-graph-v2/__tests__/handle-reactflow-on-edges-change.test.ts
@@ -212,3 +212,125 @@ test('handleReactFlowEdgesChange should remove edge', () => {
     edges: [],
   });
 });
+
+test('handleReactFlowEdgesChange should remove edge and reset the value type of the target variable of the removed edge', () => {
+  const prevState: State = {
+    ...MOCK_STATE,
+    nodes: [
+      {
+        id: 'Is8Op',
+        type: 'OutputNode',
+        position: {
+          x: 888.2297300886163,
+          y: 225.92526667211996,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 132,
+      },
+      {
+        id: 'VhWOu',
+        type: 'ElevenLabs',
+        position: {
+          x: 540.0709333333333,
+          y: 211.22993333333335,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 352,
+      },
+    ],
+    edges: [
+      {
+        source: 'VhWOu',
+        sourceHandle: 'VhWOu/audio',
+        target: 'Is8Op',
+        targetHandle: 'Is8Op/5TUFT',
+        id: 'qVJeE',
+        style: {
+          strokeWidth: 2,
+        },
+        selected: true,
+      },
+    ],
+    nodeConfigsDict: {
+      Is8Op: {
+        nodeId: 'Is8Op',
+        type: 'OutputNode',
+      },
+      VhWOu: {
+        nodeId: 'VhWOu',
+        type: 'ElevenLabs',
+        voiceId: '',
+      },
+    },
+    variablesDict: {
+      'Is8Op/5TUFT': {
+        type: 'FlowOutput',
+        id: 'Is8Op/5TUFT',
+        nodeId: 'Is8Op',
+        index: 0,
+        name: 'var2',
+        valueType: 'Audio',
+      },
+      'VhWOu/text': {
+        type: 'NodeInput',
+        id: 'VhWOu/text',
+        name: 'text',
+        nodeId: 'VhWOu',
+        index: 0,
+        valueType: 'Unknown',
+      },
+      'VhWOu/audio': {
+        type: 'NodeOutput',
+        id: 'VhWOu/audio',
+        name: 'audio',
+        nodeId: 'VhWOu',
+        index: 0,
+        valueType: 'Audio',
+      },
+      'VhWOu/H8v1p': {
+        type: 'ConditionTarget',
+        id: 'VhWOu/H8v1p',
+        nodeId: 'VhWOu',
+      },
+    },
+    variableValueLookUpDicts: [
+      {
+        'Is8Op/5TUFT': null,
+        'VhWOu/text': null,
+        'VhWOu/audio': null,
+      },
+    ],
+  };
+
+  const nextState = produce(prevState, (draft) => {
+    handleReactFlowEdgesChange(draft, {
+      type: ChangeEventType.RF_EDGES_CHANGE,
+      changes: [
+        {
+          id: 'qVJeE',
+          type: 'remove',
+        },
+      ],
+    } as BaseEvent);
+  });
+
+  expect(nextState).toEqual({
+    ...prevState,
+    edges: [],
+    variablesDict: {
+      ...prevState.variablesDict,
+      'Is8Op/5TUFT': {
+        type: 'FlowOutput',
+        id: 'Is8Op/5TUFT',
+        nodeId: 'Is8Op',
+        index: 0,
+        name: 'var2',
+        valueType: 'String',
+      },
+    },
+  });
+});
