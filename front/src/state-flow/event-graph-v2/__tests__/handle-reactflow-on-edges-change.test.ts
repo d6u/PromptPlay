b873c4f5d7512cbc2c8690d9f04236d6e2dd4cf5
Daiwei Lu <daiweilu123@gmail.com>
2024-02-24 16:54:51 -0800

    Add test for removing edge in handleReactFlowEdgesChange

--- front/src/state-flow/event-graph-v2/__tests__/handle-reactflow-on-edges-change.test.ts
+++ front/src/state-flow/event-graph-v2/__tests__/handle-reactflow-on-edges-change.test.ts
@@ -116,3 +116,99 @@ test('handleReactFlowEdgesChange should select edge', () => {
     ],
   });
 });
+
+test('handleReactFlowEdgesChange should remove edge', () => {
+  const prevState: State = {
+    ...MOCK_STATE,
+    nodes: [
+      {
+        id: 'Is8Op',
+        type: 'OutputNode',
+        position: {
+          x: 690,
+          y: 159,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 132,
+      },
+      {
+        id: 'OYlVw',
+        type: 'InputNode',
+        position: {
+          x: 321.8085333333334,
+          y: 150.6265333333333,
+        },
+        data: null,
+        dragHandle: '.node-drag-handle',
+        width: 300,
+        height: 132,
+      },
+    ],
+    edges: [
+      {
+        source: 'OYlVw',
+        sourceHandle: 'OYlVw/u4bDV',
+        target: 'Is8Op',
+        targetHandle: 'Is8Op/5TUFT',
+        id: 'F0Y38',
+        style: {
+          strokeWidth: 2,
+        },
+        selected: true,
+      },
+    ],
+    nodeConfigsDict: {
+      Is8Op: {
+        nodeId: 'Is8Op',
+        type: 'OutputNode',
+      },
+      OYlVw: {
+        nodeId: 'OYlVw',
+        type: 'InputNode',
+      },
+    },
+    variablesDict: {
+      'Is8Op/5TUFT': {
+        type: 'FlowOutput',
+        id: 'Is8Op/5TUFT',
+        nodeId: 'Is8Op',
+        index: 0,
+        name: 'var2',
+        valueType: 'String',
+      },
+      'OYlVw/u4bDV': {
+        type: 'FlowInput',
+        id: 'OYlVw/u4bDV',
+        nodeId: 'OYlVw',
+        index: 0,
+        name: 'var1',
+        valueType: 'String',
+      },
+    },
+    variableValueLookUpDicts: [
+      {
+        'Is8Op/5TUFT': null,
+        'OYlVw/u4bDV': null,
+      },
+    ],
+  };
+
+  const nextState = produce(prevState, (draft) => {
+    handleReactFlowEdgesChange(draft, {
+      type: ChangeEventType.RF_EDGES_CHANGE,
+      changes: [
+        {
+          id: 'F0Y38',
+          type: 'remove',
+        },
+      ],
+    } as BaseEvent);
+  });
+
+  expect(nextState).toEqual({
+    ...prevState,
+    edges: [],
+  });
+});
