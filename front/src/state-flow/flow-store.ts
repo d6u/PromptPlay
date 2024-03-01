ee64e12c652cb3aa0d2cc5503060a72829fec7c4
Daiwei Lu <daiweilu123@gmail.com>
2024-02-29 22:12:31 -0800

    Start actors on flow store create

--- front/src/state-flow/flow-store.ts
+++ front/src/state-flow/flow-store.ts
@@ -5,20 +5,22 @@ import { devtools } from 'zustand/middleware';
 import { createRootSlice } from './slice-root';
 import { createStateMachineActionsSlice } from './slice-state-machine-actions';
 import { FlowActions, FlowProps, StateMachineActionsStateSlice } from './types';
-import { withStateMachine } from './util/state-machine-middleware';
+import { startActors, withStateMachine } from './util/state-machine-middleware';
 
-export const useFlowStore = create(
-  devtools(
-    withStateMachine<FlowProps, FlowActions & StateMachineActionsStateSlice>(
-      withLenses((...a) => ({
-        ...createRootSlice(...a),
-        ...createStateMachineActionsSlice(...a),
-      })),
+export const useFlowStore = startActors(
+  create(
+    devtools(
+      withStateMachine<FlowProps, FlowActions & StateMachineActionsStateSlice>(
+        withLenses((...a) => ({
+          ...createRootSlice(...a),
+          ...createStateMachineActionsSlice(...a),
+        })),
+      ),
+      {
+        store: 'FlowStore',
+        // TODO: Disable for production build
+        // enabled?: boolean;
+      },
     ),
-    {
-      store: 'FlowStore',
-      // TODO: Disable for production build
-      // enabled?: boolean;
-    },
   ),
 );
