d10bbfdb7b21a158ef48affd9c965f363731073a
Daiwei Lu <daiweilu123@gmail.com>
2024-03-20 23:38:31 -0700

    Migrate auth flow to postgres

--- packages/database-models/src/index.ts
+++ packages/database-models/src/index.ts
@@ -1,6 +1,10 @@
+import { PrismaClient } from '@prisma/client';
+
 export {
   BatchTestPresetConfigDataSchemaVersion,
   CanvasDataSchemaVersion,
   PrismaClient,
   UserType,
 } from '@prisma/client';
+
+export const prismaClient = new PrismaClient();
--- server_nodejs/src/middleware/user.ts
+++ server_nodejs/src/middleware/user.ts
@@ -1,5 +1,4 @@
-import { PlaceholderUserEntity } from 'dynamodb-models/placeholder-user';
-import { UserEntity } from 'dynamodb-models/user';
+import { prismaClient } from 'database-models';
 import { NextFunction, Response } from 'express';
 import { RequestWithSession } from '../types';
 
@@ -22,13 +21,10 @@ export async function attachUser(
   const userId = req.session!.userId;
 
   if (userId != null) {
-    const { Item: user } = await UserEntity.get({ id: userId });
+    const user = await prismaClient.user.findUnique({ where: { id: userId } });
 
     if (user != null) {
-      dbUser = {
-        id: user.id,
-        isPlaceholderUser: false,
-      };
+      dbUser = { id: user.id, isPlaceholderUser: false };
     } else {
       delete req.session!.userId;
     }
@@ -38,15 +34,12 @@ export async function attachUser(
     const placeholderUserToken = req.session!.placeholderUserToken;
 
     if (placeholderUserToken != null) {
-      const { Item: user } = await PlaceholderUserEntity.get({
-        placeholderClientToken: placeholderUserToken,
+      const user = await prismaClient.user.findUnique({
+        where: { placeholderClientToken: placeholderUserToken },
       });
 
       if (user != null) {
-        dbUser = {
-          id: user.placeholderClientToken,
-          isPlaceholderUser: true,
-        };
+        dbUser = { id: user.id, isPlaceholderUser: true };
       } else {
         delete req.session!.placeholderUserToken;
       }
