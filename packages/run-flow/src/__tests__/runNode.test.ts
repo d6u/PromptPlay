7558e72db5889890f94ac09d8caa93f9a82c33bb
Daiwei Lu <daiweilu123@gmail.com>
2024-04-18 19:05:44 -0700

    Share fixtures between tests

--- packages/run-flow/src/__tests__/runNode.test.ts
+++ packages/run-flow/src/__tests__/runNode.test.ts
@@ -1,3 +1,4 @@
+import type { Edge } from 'reactflow';
 import { ReplaySubject, lastValueFrom, tap, throwError } from 'rxjs';
 import { describe, expect, test } from 'vitest';
 
@@ -7,7 +8,6 @@ import type {
   VariableValueRecords,
 } from 'flow-models';
 
-import type { Edge } from 'reactflow';
 import RunFlowContext from '../RunFlowContext';
 import type { RunNodeProgressEvent } from '../event-types';
 import { runNode } from '../runFlow';
@@ -17,6 +17,7 @@ import {
   NodeRunState,
   type RunFlowParams,
 } from '../types';
+import { createFixtureForNodeClassProcess } from './fixture';
 
 describe('Start node class', () => {
   function createFixtureForNodeClassStart() {
@@ -151,142 +152,6 @@ describe('Start node class', () => {
 });
 
 describe('Process node class', () => {
-  function createFixtureForNodeClassProcess() {
-    const edges: Edge[] = [
-      {
-        id: '84q9B',
-        source: 'PM5i4',
-        sourceHandle: 'PM5i4/hbg4s',
-        target: 'hstPg',
-        targetHandle: 'hstPg/XrU7m',
-      },
-    ];
-
-    const nodeConfigs: Record<string, NodeAllLevelConfigUnion> = {
-      PM5i4: {
-        class: 'Start',
-        type: 'InputNode',
-        nodeId: 'PM5i4',
-        nodeName: 'input1',
-      },
-      hstPg: {
-        class: 'Process',
-        type: 'TextTemplate',
-        nodeId: 'hstPg',
-        content: '',
-      },
-    };
-
-    const connectors: ConnectorRecords = {
-      'PM5i4/4zxZ6': {
-        type: 'NodeOutput',
-        id: 'PM5i4/4zxZ6',
-        name: 'start_input_val2',
-        nodeId: 'PM5i4',
-        index: 1,
-        valueType: 'String',
-        isGlobal: true,
-        globalVariableId: 'GTCdE',
-      },
-      'PM5i4/hbg4s': {
-        type: 'NodeOutput',
-        id: 'PM5i4/hbg4s',
-        name: 'start_input_val1',
-        nodeId: 'PM5i4',
-        index: 0,
-        valueType: 'String',
-        isGlobal: false,
-        globalVariableId: null,
-      },
-      'PM5i4/sMBfz': {
-        type: 'OutCondition',
-        id: 'PM5i4/sMBfz',
-        nodeId: 'PM5i4',
-        index: 0,
-        expressionString: '',
-      },
-      'hstPg/3neA2': {
-        type: 'InCondition',
-        id: 'hstPg/3neA2',
-        nodeId: 'hstPg',
-      },
-      'hstPg/I3lzc': {
-        type: 'NodeInput',
-        id: 'hstPg/I3lzc',
-        name: 'input_val4',
-        nodeId: 'hstPg',
-        index: 3,
-        valueType: 'String',
-        isGlobal: true,
-        globalVariableId: null,
-      },
-      'hstPg/Tw8g0': {
-        type: 'NodeInput',
-        id: 'hstPg/Tw8g0',
-        name: 'input_val3',
-        nodeId: 'hstPg',
-        index: 2,
-        valueType: 'String',
-        isGlobal: false,
-        globalVariableId: null,
-      },
-      'hstPg/XrU7m': {
-        type: 'NodeInput',
-        id: 'hstPg/XrU7m',
-        name: 'input_val1',
-        nodeId: 'hstPg',
-        index: 0,
-        valueType: 'String',
-        isGlobal: false,
-        globalVariableId: null,
-      },
-      'hstPg/c4Ts9': {
-        type: 'OutCondition',
-        id: 'hstPg/c4Ts9',
-        nodeId: 'hstPg',
-        index: 0,
-        expressionString: '',
-      },
-      'hstPg/g3NPR': {
-        type: 'NodeInput',
-        id: 'hstPg/g3NPR',
-        name: 'input_val2',
-        nodeId: 'hstPg',
-        index: 1,
-        valueType: 'String',
-        isGlobal: true,
-        globalVariableId: 'GTCdE',
-      },
-      'hstPg/content': {
-        type: 'NodeOutput',
-        id: 'hstPg/content',
-        name: 'content',
-        nodeId: 'hstPg',
-        index: 0,
-        valueType: 'String',
-        isGlobal: false,
-        globalVariableId: null,
-      },
-    };
-
-    const inputVariableValues: VariableValueRecords = {
-      'PM5i4/hbg4s': { value: 'test 1' },
-      'GTCdE': { value: 'test 2' },
-    };
-
-    const previousNodeNodeId = 'PM5i4';
-    const currentNodeId = 'hstPg';
-
-    return {
-      edges,
-      nodeConfigs,
-      connectors,
-      inputVariableValues,
-      previousNodeNodeId,
-      currentNodeId,
-    };
-  }
-
   test('runNode should run propagate run state in case of SUCCESS', () => {
     return new Promise<void>((resolve, reject) => {
       // SECTION: Setup
@@ -566,7 +431,7 @@ describe('Process node class', () => {
     ]);
   });
 
-  test('runNode should run emit event to progressObserver (runNode throws error case)', async () => {
+  test('runNode should run emit event to progressObserver (runNode throwed error)', async () => {
     // SECTION: Setup
     const {
       edges,
