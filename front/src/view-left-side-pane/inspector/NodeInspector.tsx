ed0d7ccb7c2db0c414938daa0fd70eb803134732
Daiwei Lu <daiweilu123@gmail.com>
2024-04-30 19:26:50 -0700

    Add NodeInspector

---
+++ front/src/view-left-side-pane/inspector/NodeInspector.tsx
@@ -0,0 +1,24 @@
+import { useFlowStore } from 'state-flow/flow-store';
+import InspectorHeader from './InspectorHeader';
+
+function NodeInspector() {
+  const nodeId = useFlowStore((s) => s.canvasLeftPaneSelectedNodeId);
+
+  const configExists = useFlowStore(
+    (s) => nodeId != null && s.getFlowContent().nodeConfigs[nodeId] != null,
+  );
+
+  // NOTE: Do this check so we don't have to check for null in every child
+  // component. This happens when the node is deleted.
+  if (!configExists) {
+    return null;
+  }
+
+  return (
+    <div>
+      <InspectorHeader nodeId={nodeId!} />
+    </div>
+  );
+}
+
+export default NodeInspector;
